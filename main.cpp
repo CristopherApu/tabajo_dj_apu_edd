2518117458ec01d560c390549d9649c332c04a15
DJerez4 <91216316+DJerez4@users.noreply.github.com>
2022-04-13 09:51:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 
 struct Cliente{
+    int Clientes;
     char nombre[30];
     int clave;
     char mail[50];
@@ -13,6 +14,7 @@ void ingresarCliente(Cliente* a);
 int main(){
     Cliente cliente;
     ingresarCliente(&cliente);
+    printf("%d\n", cliente.Clientes);
     printf("%s\n", cliente.nombre);
     printf("%s\n", cliente.mail);
     printf("%s\n", cliente.telefono);
@@ -20,6 +22,8 @@ int main(){
 }
 
 void ingresarCliente(Cliente* a){
+    printf("Ingrese la cantidad de clientes:\n");
+    scanf("%d", &(a->Clientes));
     printf("Ingrese el nombre o apodo:\n");
     scanf("%s", a->nombre);
     printf("Ingrese su email:\n");
@@ -27,3 +31,4 @@ void ingresarCliente(Cliente* a){
     printf("Ingrese su telefono:\n+ 56 9 ");
     scanf("%lu", &(a->telefono));
 }
+
