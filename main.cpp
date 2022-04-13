01fd6bb5843789bf6c433d63352fead3d73a67df
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-04-13 09:33:40 -0400

    Update main.cpp
    eliminamos la impresión de los datos

--- main.cpp
+++ main.cpp
@@ -9,16 +9,13 @@ struct Cliente{
 };
 
 void ingresarCliente(Cliente* a);
-Cliente crearCliente(); 
 
 int main(){
     Cliente cliente;
     ingresarCliente(&cliente);
     printf("%s\n", cliente.nombre);
-    printf("%d\n", cliente.clave);
-    Cliente nuevo = crearCliente();
-    printf("%s\n", nuevo.nombre);
-    printf("%d\n", nuevo.clave);
+    printf("%s\n", cliente.mail);
+    printf("%s\n", cliente.telefono);
     return 0;
 }
 
@@ -27,17 +24,6 @@ void ingresarCliente(Cliente* a){
     scanf("%s", a->nombre);
     printf("Ingrese su email:\n");
     scanf("%s", a->mail);
-    printf("Ingrese su teléfono:\n+ 56 9 ");
+    printf("Ingrese su telefono:\n+ 56 9 ");
     scanf("%lu", &(a->telefono));
-}
-
-Cliente crearCliente(){
-    Cliente a;
-    printf("Ingrese su nombre:\n");
-    scanf("%s", a.nombre);
-    printf("Ingrese su email:\n");
-    scanf("%s", a.mail);
-    printf("Ingrese su teléfono:\n");
-    scanf("%lu", &(a.telefono));
-    return a;
 }
