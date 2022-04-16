ba3d5e80e1d6c3aa4e41cb6c5404a6e3d4b29793
Cristopher <74779158+CristopherApu@users.noreply.github.com>
2022-04-16 12:42:08 -0400

    Update main.cpp
    arreglo valores del montoPorPersona

--- main.cpp
+++ main.cpp
@@ -63,12 +63,12 @@ void ingresarCliente(Cliente* a){
 
 void cuenta(){
 
-    int hora;
+    int hora = 0;
     int consumioMas = 1;
     int queConsumio;
     int montoPorPersona = 0;
-    int porcentajePropina;
-    int montoFinal;
+    int porcentajePropina = 0;
+    int montoFinal = 0;
 
     printf("Hola , que consumiste? (favor ingresar de a uno)\n");
     
@@ -77,119 +77,119 @@ void cuenta(){
     printf("Ingrese el numero a la izquierda del consumible por favor\n\n");
     printf("1    Mojito\n2    Caipirinha\n3    Laguna Azul\n4    Blue Orange\n5    Daikiri\n6    Shot de Tequila\n7    Piscola\n8    Roncola\n9    Papas Bravas\n10   Pizza\n11   Completo\n\n"); 
    
-    scanf("%i", &queConsumio);
+    scanf("%d", &queConsumio);
    
          if (queConsumio == 1){
 
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
           
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
 
         }else if (queConsumio == 2){
 
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
 
         }else if (queConsumio == 3){
 
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
 
         }else if (queConsumio == 4){
             
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
 
         }else if (queConsumio == 5){
             
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
         }else if (queConsumio == 6){
             
-          montoPorPersona + 5000;
+          montoPorPersona = montoPorPersona + 5000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 3000;
+            montoPorPersona = montoPorPersona - 3000;
 
           }
         }else if (queConsumio == 7){
             
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
         }else if (queConsumio == 8){
 
-          montoPorPersona + 3000;
+          montoPorPersona = montoPorPersona + 3000;
           
           printf("Favor ingrese la hora en la que pidio el consumible\n");
-          scanf("%i", &hora);
+          scanf("%d", &hora);
 
           if(hora < 19000){
 
-            montoPorPersona - 1500;
+            montoPorPersona = montoPorPersona - 1500;
 
           }
         }else if (queConsumio == 9){
             
-          montoPorPersona + 6000;
+          montoPorPersona = montoPorPersona + 6000;
           
         }else if (queConsumio == 10){
         
-          montoPorPersona + 12000;
+          montoPorPersona = montoPorPersona + 12000;
     
         }else if (queConsumio == 11){
             
-          montoPorPersona + 1500;
+          montoPorPersona = montoPorPersona + 1500;
           
         }else{
 
@@ -199,13 +199,13 @@ void cuenta(){
     printf("\nConsumiste algo mas?\n");
     printf("ingrese el numero a la izquierda de la respuesta por favor\n");
     printf("1   Si\n2   No\n");
-    scanf("%i", &consumioMas);
+    scanf("%d", &consumioMas);
     }
     
-    printf("Usted consumio %i CLP\n", &montoPorPersona);
     printf("De cuanto es la propina a agregar?\n%");
-    scanf("%i", &porcentajePropina);
+    scanf("%d", &porcentajePropina);
     montoFinal = ((montoPorPersona / 100) * porcentajePropina) + montoPorPersona;
-    printf("Usted debe pagar %i CLP\n", montoFinal);
+    printf("Usted debe pagar %d CLP\n", montoFinal);
+    printf("\n-------------------------------\n");
 
 }
